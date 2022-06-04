4edbe673f32d3e4f3f5de1b4cc0b16cae71cdb29
zynpkrsy <zynpkrsy1@gmail.com>
2022-06-05 01:04:21 +0300

    Add files via upload

---
+++ TaximeterProgram/src/Taximeter_Program.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+     int distanceKm, openingPrice ;
+
+     double perKm = 2.2, totalPrice;
+
+        openingPrice= 10;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Distance in Km : ");
+        distanceKm = inp.nextInt();
+
+        totalPrice = (distanceKm < 5) ? (20) : (distanceKm * perKm + openingPrice);
+
+        System.out.print("Total Price : " + totalPrice + " TL");
+
+        }
+    }
