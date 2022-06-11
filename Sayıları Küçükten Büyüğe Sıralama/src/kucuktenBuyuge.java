f2e8947fa5efb369d349c54def1377be498ad0ef
zynpkrsy <zynpkrsy1@gmail.com>
2022-06-11 05:22:24 +0300

    Add files via upload

---
+++ Sayıları Küçükten Büyüğe Sıralama/src/kucuktenBuyuge.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class kucuktenBuyuge {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a Sayýsý : ");
+        a = input.nextInt();
+
+        System.out.print("b Sayýsý : ");
+        b = input.nextInt();
+
+        System.out.print("c Sayýsý : ");
+        c = input.nextInt();
+
+        if ((a < b) && (a < c)){
+            if (b < c){
+                System.out.print("a < b < c");
+            } else {
+                System.out.print("a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.print("b < a < c");
+            } else {
+                System.out.print("b < c < a");
+            }
+        } else {
+            if (a < b) {
+                System.out.print("c < a < b");
+            } else {
+                System.out.print("c < b < a");
+            }
+        }
+    }
+}
